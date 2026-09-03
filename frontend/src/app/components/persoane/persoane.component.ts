e2474fd61435b603856d7fe4f48079fda46b1e26
Petrut Poenaru <ppoenaru@spconsultant.ro>
2026-09-03 20:46:41 +0300

    ( feature ) : frontend / add person table

--- frontend/src/app/components/persoane/persoane.component.ts
+++ frontend/src/app/components/persoane/persoane.component.ts
@@ -1,8 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Person } from '../../models/person.model';
+import { PersonService } from '../../services/person.service';
 
 @Component({
   selector: 'app-persoane',
   templateUrl: './persoane.component.html',
   styleUrls: ['./persoane.component.scss']
 })
-export class PersoaneComponent {}
+export class PersoaneComponent implements OnInit {
+  persons: Person[] = [];
+  loading = false;
+  error = '';
+
+  constructor(private personService: PersonService) {}
+
+  ngOnInit(): void {
+    this.loadPersons();
+  }
+
+  loadPersons(): void {
+    this.loading = true;
+    this.error = '';
+    this.personService.findAll().subscribe({
+      next: (persons) => {
+        this.persons = persons;
+        this.loading = false;
+      },
+      error: () => {
+        this.error = 'Nu s-au putut încărca persoanele.';
+        this.loading = false;
+      }
+    });
+  }
+}

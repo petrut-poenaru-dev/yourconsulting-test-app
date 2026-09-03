e00ceea9ed84926c368c4a2a7812e946484618e3
Petrut Poenaru <ppoenaru@spconsultant.ro>
2026-09-03 20:00:36 +0300

    ( feature ) : frontend /  cars table

--- frontend/src/app/components/masini/masini.component.ts
+++ frontend/src/app/components/masini/masini.component.ts
@@ -1,8 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Car } from '../../models/car.model';
+import { CarService } from '../../services/car.service';
 
 @Component({
   selector: 'app-masini',
   templateUrl: './masini.component.html',
   styleUrls: ['./masini.component.scss']
 })
-export class MasiniComponent {}
+export class MasiniComponent implements OnInit {
+  cars: Car[] = [];
+  loading = false;
+  error = '';
+
+  constructor(private carService: CarService) {}
+
+  ngOnInit(): void {
+    this.loadCars();
+  }
+
+  loadCars(): void {
+    this.loading = true;
+    this.error = '';
+    this.carService.findAll().subscribe({
+      next: (cars) => {
+        this.cars = cars;
+        this.loading = false;
+      },
+      error: () => {
+        this.error = 'Nu s-au putut încărca mașinile.';
+        this.loading = false;
+      }
+    });
+  }
+}
